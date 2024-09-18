3e1a48a1cf26254f058cbfb5bac9c138136906b4
lebrancconvas <lebrancconvas@gmail.com>
2024-09-18 11:45:53 +0700

    ✨ feat: add jsdoc for create memory method

--- console/src/libs/memory.ts
+++ console/src/libs/memory.ts
@@ -1,7 +1,14 @@
 export class Memory {
+  /**
+   * Memory Class for Emulator.
+   * @name create
+   * @param {number} size: Size of the memory (Byte).
+   * @returns {DataView} data: DataView object for memory buffer.
+   */
   static create(size: number) {
     const buffer = new ArrayBuffer(size);
     const data = new DataView(buffer);
     return data;
   }
+  
 };
