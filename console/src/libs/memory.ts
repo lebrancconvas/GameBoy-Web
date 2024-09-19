e7d458430eb2f69ea145bba248bc1a2d73adfcb0
Poom Yimyuean <33986491+lebrancconvas@users.noreply.github.com>
2024-09-19 03:44:49 +0000

    :sparkles: feat(console): update condition for memory size

--- console/src/libs/memory.ts
+++ console/src/libs/memory.ts
@@ -6,9 +6,11 @@ export class Memory {
    * @returns {DataView} data: DataView object for memory buffer.
    */
   static create(size: number) {
+    if(size <= 0) {
+      throw new Error("Memory size must be greater than 0.");
+    }
     const buffer = new ArrayBuffer(size);
     const data = new DataView(buffer);
     return data;
   }
-  
 };
